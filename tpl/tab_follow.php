108a36b5763a8fd8db6c09a92979fadeac6a9693
yutaro kawaguchi <kawaguchi@buff-design.com>
2021-11-27 02:46:41 +0900

    【ADD】：タブ機能追加

---
+++ tpl/tab_follow.php
@@ -0,0 +1,111 @@
+<?php 
+  $description = 'jQueryサンプルを無料配信中';
+  $keywords = 'WordPress,php,css,sacc,scss,jQuery,html,javascript';
+  $sitename = 'buff design';
+  $url = 'https://demo.buff-design.com/tpl/tab_follow.php';
+  $title = 'タブ切替';
+  $image = 'https://buff-design.com/demo/img/shutterstock_365414408.jpg';
+  include("../header.php"); 
+?>
+
+  <section class="c-tab">
+    <!-- タブ切替ボタン -->
+    <ul class="c-tab__nav">
+      <li id="tabDetail" class="c-tab__tabbar jc-tab js-active"><a href="#ji-tab-1">DESIGN</a></li>
+      <li id="tabTable" class="c-tab__tabbar jc-tab"><a href="#ji-tab-2">CODING</a></li>
+    </ul>
+
+    <!-- 案件ボックス・比較表タブ切替追従ボタン用 -->
+    <nav id="ji-tabNavi" class="c-tab-navi">
+      <ul class="c-tab-navi__inner">
+        <li id="tabNaviDetail" class="c-tab-navi__button jc-tab02 js-active"><a href="#ji-tab-1">DESIGN</a></li>
+        <li id="tabNaviTable" class="c-tab-navi__button jc-tab02"><a href="#ji-tab-2">CODING</a></li>
+      </ul>
+    </nav>
+    
+    <section id="ji-tabContainer" class="c-tab__wrapper">
+      <!-- タブ切替ボタン1 -->
+      <article id="ji-tab-1" class="c-tab__content jc_tabContent02 js-show">
+        <ul class="c-tab__inner">
+          <li class="c-tab__list">
+            <div class="c-tab__list-title">UI/UXデザイン</div>
+            <div class="c-tab__list-image">
+              <img src="https://buff-design.com/demo/img/shutterstock_364944905.jpg">
+            </div>
+            <p class="c-tab__list-text">ユーザーエクスペリエンスデザインは、製品との対話で提供されるユーザビリティ、有用性、および望ましさを通じてユーザーの行動をサポートするプロセスです。</p>
+          </li>
+          <li class="c-tab__list">
+            <div class="c-tab__list-title">UI/UXデザイン</div>
+            <div class="c-tab__list-image">
+              <img src="https://buff-design.com/demo/img/shutterstock_364944905.jpg">
+            </div>
+            <p class="c-tab__list-text">ユーザーエクスペリエンスデザインは、製品との対話で提供されるユーザビリティ、有用性、および望ましさを通じてユーザーの行動をサポートするプロセスです。</p>
+          </li>
+          <li class="c-tab__list">
+            <div class="c-tab__list-title">UI/UXデザイン</div>
+            <div class="c-tab__list-image">
+              <img src="https://buff-design.com/demo/img/shutterstock_364944905.jpg">
+            </div>
+            <p class="c-tab__list-text">ユーザーエクスペリエンスデザインは、製品との対話で提供されるユーザビリティ、有用性、および望ましさを通じてユーザーの行動をサポートするプロセスです。</p>
+          </li>
+          <li class="c-tab__list">
+            <div class="c-tab__list-title">UI/UXデザイン</div>
+            <div class="c-tab__list-image">
+              <img src="https://buff-design.com/demo/img/shutterstock_364944905.jpg">
+            </div>
+            <p class="c-tab__list-text">ユーザーエクスペリエンスデザインは、製品との対話で提供されるユーザビリティ、有用性、および望ましさを通じてユーザーの行動をサポートするプロセスです。</p>
+          </li>
+          <li class="c-tab__list">
+            <div class="c-tab__list-title">UI/UXデザイン</div>
+            <div class="c-tab__list-image">
+              <img src="https://buff-design.com/demo/img/shutterstock_364944905.jpg">
+            </div>
+            <p class="c-tab__list-text">ユーザーエクスペリエンスデザインは、製品との対話で提供されるユーザビリティ、有用性、および望ましさを通じてユーザーの行動をサポートするプロセスです。</p>
+          </li>
+        </ul>
+      </article>
+      <!-- タブ切替ボタン2 -->
+      <article id="ji-tab-2" class="c-tab__content jc_tabContent02">
+        <ul class="c-tab__inner">
+          <li class="c-tab__list">
+            <div class="c-tab__list-title">CODING</div>
+            <div class="c-tab__list-image">
+            <img src="https://buff-design.com/demo/img/shutterstock_365414408.jpg">
+            </div>
+            <p class="c-tab__list-text">コーディングはコンピューターに指示や命令を出す際に、「コンピューターが理解できる言語でコードを書くこと」です。</p>
+          </li>
+          <li class="c-tab__list">
+            <div class="c-tab__list-title">CODING</div>
+            <div class="c-tab__list-image">
+            <img src="https://buff-design.com/demo/img/shutterstock_365414408.jpg">
+            </div>
+            <p class="c-tab__list-text">コーディングはコンピューターに指示や命令を出す際に、「コンピューターが理解できる言語でコードを書くこと」です。</p>
+          </li>
+          <li class="c-tab__list">
+            <div class="c-tab__list-title">CODING</div>
+            <div class="c-tab__list-image">
+            <img src="https://buff-design.com/demo/img/shutterstock_365414408.jpg">
+            </div>
+            <p class="c-tab__list-text">コーディングはコンピューターに指示や命令を出す際に、「コンピューターが理解できる言語でコードを書くこと」です。</p>
+          </li>
+          <li class="c-tab__list">
+            <div class="c-tab__list-title">CODING</div>
+            <div class="c-tab__list-image">
+            <img src="https://buff-design.com/demo/img/shutterstock_365414408.jpg">
+            </div>
+            <p class="c-tab__list-text">コーディングはコンピューターに指示や命令を出す際に、「コンピューターが理解できる言語でコードを書くこと」です。</p>
+          </li>
+          <li class="c-tab__list">
+            <div class="c-tab__list-title">CODING</div>
+            <div class="c-tab__list-image">
+            <img src="https://buff-design.com/demo/img/shutterstock_365414408.jpg">
+            </div>
+            <p class="c-tab__list-text">コーディングはコンピューターに指示や命令を出す際に、「コンピューターが理解できる言語でコードを書くこと」です。</p>
+          </li>
+        </ul>
+      </article>
+    </section>
+
+  </section>
+  <script src="../js/tab.js"></script>
+<?php include("../footer.php"); ?>
